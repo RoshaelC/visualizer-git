712af8dea305e3569548d01a319dfb1365bc3034
RoshaelC <rosche2@outlook.com>
2022-01-19 20:15:58 -0500

    Add source code to project

---
+++ VisualizeR Code.R
@@ -0,0 +1,31 @@
+# Visualize-R
+# Roshael Chellappah
+# BIOL 434 - Dr. Collauti
+# January 19th, 2022
+
+# see word document "Visualize-R Part I.docx" for Part I of assignment.
+
+# PART 2 - VISUALIZATIONS
+# **Data Overview**
+
+# Dong, Lei et al. (2020), Data from: Phylogenetic structure and formation mechanism of shrub 
+# communities in arid and semi-arid areas of the Mongolian Plateau, Dryad, Dataset, 
+# https://doi.org/10.5061/dryad.6m905qfvv
+
+# This paper studied the phylogenetic structure of the shrub communities in arid and semiarid 
+# areas of the Mongolian Plateau. The main variables within the dataset "Traits.csv" are the 
+# phylogenetic signals of the height(cm), canopy (m2), leaf length (cm) and leaf width (mm) of 
+# studied shrubs and subshrubs.
+
+
+
+
+
+
+
+
+
+
+
+
+
